b5873fa9cce10570fbcd1fe5e97283f455784a67
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-15 11:21:08 +0700

    Update Switch Locale Style.

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -11,19 +11,19 @@
                     <ul class="group text-center flex flex-1 list-none items-center justify-center gap-x-5">
                         <li>
                             <button
-                                class="group font-noto_sans text-lg  inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                                class="group uppercase font-bold font-noto_sans text-lg  inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none">
                                 {{ $t('about_me') }}
                             </button>
                         </li>
                         <li>
                             <button
-                                class="group inline-flex font-noto_sans h-10 w-max items-center justify-center rounded-md px-4 py-2 text-lg transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                                class="group  uppercase font-bold  inline-flex font-noto_sans h-10 w-max items-center justify-center rounded-md px-4 py-2 text-lg transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none">
                                 {{ $t('projects') }}
                             </button>
                         </li>
                         <li>
                             <button
-                                class="group inline-flex font-noto_sans h-10 w-max items-center justify-center rounded-md px-4 py-2 text-lg transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                                class="group  uppercase font-bold  inline-flex font-noto_sans h-10 w-max items-center justify-center rounded-md px-4 py-2 text-lg transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none">
                                 {{ $t('blog') }}
 
                             </button>
