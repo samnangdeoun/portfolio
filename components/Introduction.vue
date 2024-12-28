cc1b1f681e4f2d9d0cce68d6029f3f68a5b9c614
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-28 23:56:47 +0700

    Responsive

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex justify-around items-center p-[1rem] gap-[1rem]">
-        <div class="w-1/4">
+        <div class="w-1/4 hidden lg:block">
             <Sidebar />
         </div>
         <div
-            class="overflow-y-scroll border w-3/4 bg-white dark:bg-[#091a28] dark:text-white shadow-lg rounded-2xl h-[calc(100vh-7rem)]">
+            class="overflow-y-scroll border w-full lg:w-3/4  bg-white dark:bg-[#091a28] dark:text-white shadow-lg rounded-2xl h-[calc(100vh-7rem)]">
             <RouterView />
         </div>
     </div>
